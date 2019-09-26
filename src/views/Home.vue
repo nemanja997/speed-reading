f3e9bbe9eb294a1fe619fa446f1885dfc46d177f
Nemanja <kazicnemanja1@gmail.com>
2019-09-26 13:14:52 +0200

    Edit default Vue CLI template

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,21 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="bg-light p-5"  style="height:500px;">
+      <form action="" class=" h-100">
+        <div class="form-group h-100">
+          
+          <textarea class="form-control h-100" id="insertText"></textarea>
+        </div>
+      </form>
+     
+    </div>
   </div>
 </template>
-
+<style lang="scss">
+  #insertText {
+    resize:none;
+  }
+</style>
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
