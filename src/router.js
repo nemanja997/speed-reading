a32dba8e01df52437e239e9b9d03a40c8315920b
Nemanja <kazicnemanja1@gmail.com>
2019-10-03 20:45:23 +0200

    Clean up router file, remove about route and renamed other routes.

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import SettingsView from './views/SettingsView.vue'
+import ReadView from './views/ReadView.vue'
 
 Vue.use(Router)
 
@@ -10,16 +11,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
+      name: 'settings',
+      component: SettingsView
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+        path: '/read',
+        name: 'read',
+        component: ReadView
     }
   ]
 })
