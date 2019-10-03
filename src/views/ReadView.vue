d138206c7ec2850b8ed2ee7ecbbfe7828d63efff
Nemanja <kazicnemanja1@gmail.com>
2019-10-03 20:43:52 +0200

    Add reading view.

---
+++ src/views/ReadView.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <div class="bg-light p-4 read-container">
+            <span class="read-text" v-text="chunk">Text asdasdasd asdasdasdas</span>
+        </div>
+    </div>
+</template>
+
+<script>
+    import {mapState} from 'vuex';
+    export default {
+        name: "Read",
+        data(){
+            return{
+                chunk:''
+            }
+        },
+        computed:{
+        ...mapState({
+            text: state => state.text,
+            })
+        },
+        methods:{
+            startReading(){
+
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .read-container{
+        height:500px;
+        text-align:center;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+    }
+    .read-text{
+        color:black;
+        font-size:2.5em;
+    }
+</style>
