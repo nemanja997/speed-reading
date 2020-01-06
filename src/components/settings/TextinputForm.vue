a290b35e9f7e61c9fed7c337029bed2f0273103b
Nemanja <kazicnemanja1@gmail.com>
2020-01-06 19:35:24 +0100

    Take custom text from state.

--- src/components/settings/TextinputForm.vue
+++ src/components/settings/TextinputForm.vue
@@ -22,20 +22,22 @@
 </template>
 
 <script>
-
+    import {mapState} from 'vuex';
 
     export default {
         name: "TextinputForm",
         data() {
             return {
-                text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed ' +
-                    'do eiusmod tempor incididunt ut labore et dolore magna aliqua. ' +
-                    'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris ' +
-                    'nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit ' +
-                    'in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ' +
-                    'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui ' +
-                    'officia deserunt mollit anim id est laborum.',
+                initialText: '',
             }
+        },
+        mounted(){
+            this.initialText = this.text;
+        },
+        computed:{
+            ...mapState({
+                text: state => state.text
+            }),
         },
         methods: {
             start() {
--- src/store.js
+++ src/store.js
@@ -5,7 +5,13 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
     state: {
-        readingText: '',
+        readingText: '\'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed \' +\n' +
+            '                    \'do eiusmod tempor incididunt ut labore et dolore magna aliqua. \' +\n' +
+            '                    \'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris \' +\n' +
+            '                    \'nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit \' +\n' +
+            '                    \'in voluptate velit esse cillum dolore eu fugiat nulla pariatur. \' +\n' +
+            '                    \'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui \' +\n' +
+            '                    \'officia deserunt mollit anim id est laborum.\'',
         settings: {
             wordsPerMinute: 200,
             chunks: 1,
